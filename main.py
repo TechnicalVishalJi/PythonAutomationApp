183d318c38b45dd6e7ff3a6a6de8fac0a2631052
Vishal Singh <73434540+TechnicalVishalJi@users.noreply.github.com>
2024-07-09 06:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,28 @@ def add_90_min_mc_server():
 
 
 
+
+def start_chrome_driver(optionsList):
+    options = ChromeOptions()  # Change to ChromeOptions
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)  # Change to webdriver.Chrome
+    
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-background-networking")
+    options.add_argument("--disable-sync")
+    options.add_argument("--disable-translate")
+    options.add_argument("--disable-default-apps")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-background-timer-throttling")
+    options.add_argument("--disable-renderer-backgrounding")
+    
+
 def backup_minecraft_server():
     pass
 
